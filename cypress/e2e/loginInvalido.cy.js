6d6eb6a632b2b6ee72648e55de143be5380e1659
Leonardo Guiato <123307371+LeoguiatoM5@users.noreply.github.com>
2025-06-04 08:45:07 -0300

    alteracoes_03/06

--- cypress/e2e/loginInvalido.cy.js
+++ cypress/e2e/loginInvalido.cy.js
@@ -24,24 +24,3 @@ describe("Teste Login", () => {
 });
 
 
-describe("Fluxo após login válido", () => {
-  before(() => {
-    cy.visit("https://homologacaoesp.interplayers.com.br/PRJ/Especialidades/Acesso.aspx");
-    cy.get('#ContentPlaceHolder1_Control_Login2_Login1_UserName')
-      .type("admbayer@dnaspecialty.com.br");
-
-    cy.get("#ContentPlaceHolder1_Control_Login2_Login1_Password")
-      .type("12345678");
-
-    cy.get("#ContentPlaceHolder1_Control_Login2_Login1_LoginButton")
-      .click();
-  });
-
-  it("Cadastro de Cliente", () => {
-    cy.get('#nav > :nth-child(3) > .sf-with-ul').click()
-    cy.contains('Cadastro de Cliente').click();
-    
-    
-    
-  });
-});
