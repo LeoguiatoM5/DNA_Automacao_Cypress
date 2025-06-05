912e9cec71d70e85c04226a61c0978530890e5c4
Leonardo Guiato <123307371+LeoguiatoM5@users.noreply.github.com>
2025-06-05 09:14:01 -0300

    alteracoes_05/06

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,19 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://homologacaoesp.interplayers.com.br',
-    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
-    supportFile: 'cypress/support/e2e.js',
-    setupNodeEvents(on, config) {
-    
-    }
+    baseUrl: "https://homologacaoesp.interplayers.com.br",
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
+    supportFile: "cypress/support/e2e.js",
+    setupNodeEvents(on, config) {},
   },
-  reporter: 'mochawesome',
+  video: true, // Grava vídeo sempre
+  videoUploadOnPasses: false, // Mantém vídeo mesmo quando passa
+  reporter: "mochawesome",
   reporterOptions: {
-    reportDir: 'cypress/reports/html',
+    reportDir: "cypress/reports/html",
     overwrite: true,
     html: true,
-    json: false
-  }
+    json: true,
+  },
 });
